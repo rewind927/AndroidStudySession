e13f4ca217cd6cc382c8e11aeba91e1383c0d668
ethanshen <ethanshen@kkbox.com>
2015-06-12 12:38:52 +0800

    Add circle progress bar practice to NumberProgressBar project

--- NumberProgressBar/src/main/java/com/daimajia/numberprogressbar/example/NumberProgressBarActivity.java
+++ NumberProgressBar/src/main/java/com/daimajia/numberprogressbar/example/NumberProgressBarActivity.java
@@ -6,7 +6,6 @@ import android.widget.Toast;
 
 import com.daimajia.numberprogressbar.NumberProgressBar;
 import com.daimajia.numberprogressbar.OnProgressBarListener;
-import com.widget.circleprogressbar.CircleProgressBar;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -15,7 +14,6 @@ public class NumberProgressBarActivity extends ActionBarActivity implements OnPr
 	private Timer timer;
 
 	private NumberProgressBar progressBar;
-	private CircleProgressBar circleProgressBar;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -24,15 +22,6 @@ public class NumberProgressBarActivity extends ActionBarActivity implements OnPr
 
 		progressBar = (NumberProgressBar) findViewById(R.id.numberbar1);
 		progressBar.setOnProgressBarListener(this);
-		circleProgressBar = (CircleProgressBar) findViewById(R.id.circle_progress_bar);
-		circleProgressBar.setProgressBarListener(new OnProgressBarListener() {
-			@Override
-			public void onProgressChange(int current, int max) {
-				if (current == max) {
-					circleProgressBar.setProgress(0);
-				}
-			}
-		});
 		timer = new Timer();
 		timer.schedule(new TimerTask() {
 			@Override
@@ -41,7 +30,6 @@ public class NumberProgressBarActivity extends ActionBarActivity implements OnPr
 					@Override
 					public void run() {
 						progressBar.incrementProgressBy(1);
-						circleProgressBar.incrementProgressBy(1);
 					}
 				});
 			}
--- NumberProgressBar/src/main/java/com/widget/circleprogressbar/CircleProgressBar.java
+++ NumberProgressBar/src/main/java/com/widget/circleprogressbar/CircleProgressBar.java
@@ -20,7 +20,7 @@ public class CircleProgressBar extends View {
 	private int progressReachedColor;
 	private int progressUnreachedStroke;
 	private int progressReachedStroke;
-	private int progressBarCircleSize;
+	private int progressBarSize;
 	private int progressTextSize;
 	private int progressTextColor;
 	private Paint textPaint;
@@ -38,16 +38,13 @@ public class CircleProgressBar extends View {
 		try {
 			progress = a.getInt(R.styleable.CircleProgressBar_circle_progress, 0);
 			max = a.getInt(R.styleable.CircleProgressBar_circle_max, 100);
-			progressUnreachedColor = a.getColor(R.styleable.CircleProgressBar_circle_progress_unreached_color, 0xFFCCCCCC);
-			progressReachedColor = a.getColor(R.styleable.CircleProgressBar_circle_progress_reached_color, 0xFF3498DB);
-			progressUnreachedStroke = a.getDimensionPixelSize(R.styleable.CircleProgressBar_circle_progress_unreached_stroke, (int) dp2px(6));
-			progressReachedStroke = a.getDimensionPixelSize(R.styleable.CircleProgressBar_circle_progress_reached_stroke, (int) dp2px(6));
-			progressBarCircleSize = a.getDimensionPixelSize(R.styleable.CircleProgressBar_circle_progress_bar_circle_size, (int) dp2px(100));
-			progressTextSize = a.getDimensionPixelSize(R.styleable.CircleProgressBar_circle_progress_text_size, (int) sp2px(14));
-			progressTextColor = a.getColor(R.styleable.CircleProgressBar_circle_progress_text_color, 0xFF000000);
+			// TODO: get attributes .....
+
+
 		} finally {
 			a.recycle();
 		}
+		// initial paint
 		textPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
 		textPaint.setTextSize(progressTextSize);
 		textPaint.setColor(progressTextColor);
@@ -61,64 +58,19 @@ public class CircleProgressBar extends View {
 	}
 
 	@Override
-	public int getMinimumHeight() {
-		return Math.max(progressTextSize, progressBarCircleSize);
-	}
-
-	@Override
-	public int getMinimumWidth() {
-		return Math.max(progressTextSize, progressBarCircleSize);
-	}
-
-	@Override
 	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
 		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-		setMeasuredDimension(measure(widthMeasureSpec, true), measure(heightMeasureSpec, false));
-	}
+		// TODO: setMeasuredDimension .....
 
-	private int measure(int measureSpec, boolean isWidth) {
-		int result;
-		int size = MeasureSpec.getSize(measureSpec);
-		int mode = MeasureSpec.getMode(measureSpec);
-		int padding = isWidth ? getPaddingLeft() + getPaddingRight() : getPaddingTop() + getPaddingBottom();
-		if (mode == MeasureSpec.EXACTLY) {
-			result = size;
-		} else {
-			result = isWidth ? getSuggestedMinimumWidth() : getSuggestedMinimumHeight();
-			result += padding;
-			if (mode == MeasureSpec.AT_MOST) {
-				result = Math.max(result, size);
-			}
-		}
-		return result;
+
 	}
 
 	@Override
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
-		final float angel = 360 * progress / max;
+		// TODO: [tips] canvas.drawArc(reachedOval, -90, angel, false, progressReachedPaint);
 
-		RectF unreachedOval = new RectF(getPaddingLeft() + progressUnreachedStroke / 2,
-				getPaddingTop() + progressUnreachedStroke / 2,
-				getPaddingLeft() + progressBarCircleSize + progressUnreachedStroke,
-				getPaddingTop() + progressBarCircleSize + progressUnreachedStroke);
-		canvas.drawArc(unreachedOval, angel - 90, 360 - angel, false, progressUnreachedPaint);
 
-		RectF reachedOval = new RectF(getPaddingLeft() + progressReachedStroke / 2,
-				getPaddingTop() + progressReachedStroke / 2,
-				getPaddingLeft() + progressBarCircleSize + progressReachedStroke,
-				getPaddingTop() + progressBarCircleSize + progressReachedStroke);
-
-		final int reachedColor = adjustAlpha(progressReachedColor, (255 * progress / (float) max) / 255);
-		progressReachedPaint.setColor(reachedColor);
-		canvas.drawArc(reachedOval, -90, angel, false, progressReachedPaint);
-
-		final int radius = progressBarCircleSize / 2;
-		final int centerX = getPaddingLeft() + radius;
-		final int centerY = getPaddingTop() + radius;
-		final float textWidth = textPaint.measureText(progress + "");
-		final float textHeight = textPaint.ascent() + textPaint.descent();
-		canvas.drawText(progress + " %", centerX - textWidth / 2, centerY - textHeight / 2, textPaint);
 	}
 
 	public void setProgressBarListener(OnProgressBarListener listener) {
