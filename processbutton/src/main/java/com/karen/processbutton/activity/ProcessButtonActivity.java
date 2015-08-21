901e6dcf0decb18ca847abff01da63418c592810
KarenLin <karenlin@kkbox.com>
2015-08-21 14:14:15 +0800

    Add practice.

--- processbutton/src/main/java/com/karen/processbutton/activity/ProcessButtonActivity.java
+++ processbutton/src/main/java/com/karen/processbutton/activity/ProcessButtonActivity.java
@@ -6,15 +6,20 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.karen.processbutton.ActionProcessButton;
+import com.karen.processbutton.PracticProcessButton;
+import com.karen.processbutton.ProcessButton;
 import com.karen.processbutton.R;
 import com.karen.processbutton.util.ProgressGenerator;
 
 public class ProcessButtonActivity extends ActionBarActivity implements ProgressGenerator.OnCompleteListener {
 
+	private TextView labelMode;
 	private ActionProcessButton buttonSignIn;
+	private PracticProcessButton buttonPractice;
 	private EditText textEmail;
 	private EditText textPassword;
 	private ProgressGenerator progressGenerator;
@@ -27,22 +32,35 @@ public class ProcessButtonActivity extends ActionBarActivity implements Progress
 			textPassword.setEnabled(false);
 		}
 	};
+	private View.OnClickListener buttonLoginClickListener = new View.OnClickListener() {
+		@Override
+		public void onClick(View view) {
+			progressGenerator.start(buttonPractice);
+			buttonPractice.setEnabled(false);
+			textEmail.setEnabled(false);
+			textPassword.setEnabled(false);
+		}
+	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_process_button);
+		labelMode = (TextView) findViewById(R.id.label_mode);
 		textEmail = (EditText) findViewById(R.id.text_email);
 		textPassword = (EditText) findViewById(R.id.text_password);
+
 		buttonSignIn = (ActionProcessButton) findViewById(R.id.button_sign_in);
 		buttonSignIn.setMode(ActionProcessButton.Mode.PROGRESS);
 		progressGenerator = new ProgressGenerator(this);
 		buttonSignIn.setOnClickListener(buttonSignInClickListener);
+
+		buttonPractice = (PracticProcessButton) findViewById(R.id.button_practice);
+		buttonPractice.setOnClickListener(buttonLoginClickListener);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.menu_process_button, menu);
 		return true;
 	}
@@ -51,13 +69,24 @@ public class ProcessButtonActivity extends ActionBarActivity implements Progress
 	public boolean onOptionsItemSelected(MenuItem item) {
 		int id = item.getItemId();
 		if (id == R.id.progress_mode) {
-			refreshUI(false);
+			labelMode.setText(getResources().getString(R.string.progress_mode));
+			buttonSignIn.setVisibility(View.VISIBLE);
+			buttonSignIn.setMode(ActionProcessButton.Mode.PROGRESS);
+			buttonPractice.setVisibility(View.GONE);
+			refreshButtonUI(buttonSignIn);
 			return true;
 		} else if (id == R.id.endless_mode) {
-			refreshUI(true);
+			labelMode.setText(getResources().getString(R.string.endless_mode));
+			buttonSignIn.setVisibility(View.VISIBLE);
+			buttonSignIn.setMode(ActionProcessButton.Mode.ENDLESS);
+			buttonPractice.setVisibility(View.GONE);
+			refreshButtonUI(buttonSignIn);
 			return true;
 		} else if (id == R.id.practice_mode) {
-			refreshUI(true);
+			labelMode.setText(getResources().getString(R.string.practice_mode));
+			buttonSignIn.setVisibility(View.GONE);
+			buttonPractice.setVisibility(View.VISIBLE);
+			refreshButtonUI(buttonPractice);
 			return true;
 		}
 
@@ -69,14 +98,9 @@ public class ProcessButtonActivity extends ActionBarActivity implements Progress
 		Toast.makeText(this, R.string.loading_Complete, Toast.LENGTH_LONG).show();
 	}
 
-	private void refreshUI(Boolean isEndlessMode) {
-		if (!isEndlessMode) {
-			buttonSignIn.setMode(ActionProcessButton.Mode.PROGRESS);
-		} else {
-			buttonSignIn.setMode(ActionProcessButton.Mode.ENDLESS);
-		}
-		buttonSignIn.setProgress(0);
-		buttonSignIn.setEnabled(true);
+	private void refreshButtonUI(ProcessButton button) {
+		button.setProgress(0);
+		button.setEnabled(true);
 		textEmail.setEnabled(true);
 		textPassword.setEnabled(true);
 		progressGenerator = new ProgressGenerator(this);
